d82359d211e3462deb653a413cef8bb0bafd62a0
Masahiro Masuda <masahi129@gmail.com>
2020-02-16 15:10:40 +0900

    add detection model to test (but missing many conversion)

--- torchvision_test.py
+++ torchvision_test.py
@@ -17,29 +17,24 @@ class SegmentationModelWrapper(torch.nn.Module):
         return out["out"]
 
 
-def dict_to_tuple(out_dict):
-    if "masks" in out_dict.keys():
-        return (out_dict["boxes"], out_dict["scores"], out_dict["labels"], out_dict["masks"])
-    return (out_dict["boxes"], out_dict["scores"], out_dict["labels"])
-
-
 class DetectionModelWrapper(torch.nn.Module):
-    def __init__(self, model):
+    def __init__(self, model, output_key):
         super().__init__()
         self.model = model
+        self.output_key = output_key
 
     def forward(self, inp):
         out = self.model(inp)
-        return dict_to_tuple(out[0])
+        return out[0][self.output_key]
 
 
 def run_on_models(models, inp, input_shapes, target="llvm"):
     for raw_model in models:
-        script_module = torch.jit.trace(raw_model, inp).eval()
-        mod, params = parse_script_module(script_module, input_shapes)
-
         with torch.no_grad():
             pt_result = raw_model(inp).numpy()
+            script_module = torch.jit.trace(raw_model, inp).eval()
+
+        mod, params = parse_script_module(script_module, input_shapes)
 
         with relay.build_config(opt_level=3):
             json, lib, params = relay.build(mod, target=target, params=params)
@@ -65,14 +60,14 @@ def imagenet_test():
     input_shapes = {input_name: (1, 3, 224, 224)}
 
     test_models = [
-        # models.resnet.resnet18(pretrained=True).eval(),
+        models.resnet.resnet18(pretrained=True).eval(),
         models.mobilenet.mobilenet_v2(pretrained=True).eval(),
-        # models.squeezenet.squeezenet1_1(pretrained=True).eval(),
-        # models.densenet.densenet121(pretrained=True).eval(),
-        # models.inception.inception_v3(pretrained=True).eval(),
-        # models.mnasnet.mnasnet1_0(pretrained=True).eval(),
-        # models.alexnet(pretrained=True).eval(),
-        # models.vgg.vgg11_bn(pretrained=True).eval(),
+        models.squeezenet.squeezenet1_1(pretrained=True).eval(),
+        models.densenet.densenet121(pretrained=True).eval(),
+        models.inception.inception_v3(pretrained=True).eval(),
+        models.mnasnet.mnasnet1_0(pretrained=True).eval(),
+        models.alexnet(pretrained=True).eval(),
+        models.vgg.vgg11_bn(pretrained=True).eval(),
     ]
 
     for target in ["llvm"]:
@@ -102,15 +97,16 @@ def detection_test():
     inp = torch.rand(input_shapes[input_name], dtype=torch.float)
 
     test_models = []
-    for model_func in [models.detection.fasterrcnn_resnet50_fpn,
-                       models.detection.maskrcnn_resnet50_fpn]:
+    for model_func, output_key in zip([models.detection.maskrcnn_resnet50_fpn,
+                                       models.detection.fasterrcnn_resnet50_fpn],
+                                      ["masks", "boxes"]):
         detection_model = model_func(num_classes=50, pretrained_backbone=False)
-        test_models.append(DetectionModelWrapper(detection_model))
+        test_models.append(DetectionModelWrapper(detection_model.eval(), output_key))
 
-    for target in ["llvm", "cuda"]:
-        run_on_models(test_models, inp, input_shapes, target)
+    # for target in ["llvm", "cuda"]:
+    #     run_on_models(test_models, inp, input_shapes, target)
 
 
-# imagenet_test()
-# segmentation_test()
+imagenet_test()
+segmentation_test()
 detection_test()
