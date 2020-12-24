c99b852bd141ad309fde0554c564ef34749616d0
Masahiro Masuda <masahi129@gmail.com>
2020-12-24 19:12:47 +0900

    update maskrcnn_test

--- maskrcnn_test.py
+++ maskrcnn_test.py
@@ -1,25 +1,31 @@
+import tvm
+from tvm import relay, auto_scheduler
+from tvm.runtime import profiler_vm
+from tvm.runtime.vm import VirtualMachine
+from tvm.contrib.download import download
+
+import numpy as np
+import cv2
+
+# PyTorch imports
 import torch
 import torchvision
-import numpy as np
-from tvm import relay
+
+in_size = 300
+
+input_shape = (1, 3, in_size, in_size)
 
 
-def do_script(model, in_size=100):
-    model_script = torch.jit.script(model)
-    model_script.eval()
-    return model_script
-
-
-def do_trace(model, in_size=100):
-    model_trace = torch.jit.trace(model, torch.rand(1, 3, in_size, in_size))
+def do_trace(model, inp):
+    model_trace = torch.jit.trace(model, inp)
     model_trace.eval()
     return model_trace
 
 
 def dict_to_tuple(out_dict):
     if "masks" in out_dict.keys():
-        return (out_dict["boxes"], out_dict["scores"], out_dict["labels"], out_dict["masks"])
-    return (out_dict["boxes"], out_dict["scores"], out_dict["labels"])
+        return out_dict["boxes"], out_dict["scores"], out_dict["labels"], out_dict["masks"]
+    return out_dict["boxes"], out_dict["scores"], out_dict["labels"]
 
 
 class TraceWrapper(torch.nn.Module):
@@ -32,59 +38,108 @@ class TraceWrapper(torch.nn.Module):
         return dict_to_tuple(out[0])
 
 
-def save_jit_model(script=False):
-    model_funcs = [torchvision.models.detection.fasterrcnn_resnet50_fpn,
-                   torchvision.models.detection.maskrcnn_resnet50_fpn]
+def benchmark_torch(model, inp, num_iters):
+    model.to("cuda")
+    inp = inp.to("cuda")
 
-    names = ["faster_rcnn", "mask_rcnn"]
+    for i in range(3):
+        model(inp)
+    torch.cuda.synchronize()
 
-    for name, model_func in zip(names, model_funcs):
-        if script:
-            model = model_func(num_classes=50, pretrained_backbone=False)
-        else:
-            model = TraceWrapper(model_func(num_classes=50, pretrained_backbone=False))
+    import time
+    t1 = time.time()
+    for i in range(num_iters):
+        model(inp)
+    torch.cuda.synchronize()
+    t2 = time.time()
 
-        model.eval()
-        in_size = 100
-        inp = torch.rand(1, 3, in_size, in_size)
-
-        with torch.no_grad():
-            out = model(inp)
-
-            if script:
-                out = dict_to_tuple(out[0])
-                script_module = do_script(model)
-                script_out = script_module([inp[0]])[1]
-                script_out = dict_to_tuple(script_out[0])
-            else:
-                script_module = do_trace(model)
-                script_out = script_module(inp)
-
-            assert len(out[0]) > 0 and len(script_out[0]) > 0
-
-            # compare bbox coord
-            print(np.max(np.abs(out[0].numpy() - script_out[0].numpy())))
-
-            torch._C._jit_pass_inline(script_module.graph)
-            torch.jit.save(script_module, name + ".pt")
+    print("torch elapsed", (t2 - t1) / num_iters)
 
 
-def convert_roi_align():
-    def _impl(inputs, input_types):
-        spatial_scale = inputs[2]
-        pooled_size = (inputs[3], inputs[4])
-        sampling_ratio = inputs[5]
-        return relay.op.vision.roi_align(inputs[0], inputs[1],
-                                         pooled_size, spatial_scale,
-                                         sampling_ratio)
-    return _impl
+def get_input(in_size):
+    img_path = "test_street_small.jpg"
+    img_url = (
+        "https://raw.githubusercontent.com/dmlc/web-data/" "master/gluoncv/detection/street_small.jpg"
+    )
+    download(img_url, img_path)
+
+    img = cv2.imread(img_path).astype("float32")
+    img = cv2.resize(img, (in_size, in_size))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = np.transpose(img / 255.0, [2, 0, 1])
+    img = np.expand_dims(img, axis=0)
+    return img
 
 
-save_jit_model()
-script_module = torch.jit.load("mask_rcnn.pt")
-input_shapes = [("input", (1, 3, 300, 300))]
-custom_map = {'torchvision::roi_align': convert_roi_align()}
-relay.frontend.from_pytorch(script_module, input_shapes, custom_map)
-"""
-NotImplementedError: The following operators are not implemented: ['aten::__and__', 'aten::nonzero', 'aten::__interpolate', 'aten::index', 'torchvision::nms', 'aten::unbind', 'aten::scalar_tensor', 'aten::_shape_as_tensor', 'aten::scatter']
-"""
+num_iters = 50
+
+model_func = torchvision.models.detection.maskrcnn_resnet50_fpn
+model = TraceWrapper(model_func(pretrained=True, rpn_pre_nms_top_n_test=200))
+
+model.eval()
+img = get_input(in_size)
+inp = torch.from_numpy(img)
+
+with torch.no_grad():
+    out = model(inp)
+    script_module = do_trace(model, inp)
+
+
+def auto_schedule():
+    input_name = "input0"
+    shape_list = [(input_name, input_shape)]
+    mod, params = relay.frontend.from_pytorch(script_module, shape_list)
+
+    target = "cuda"
+
+    tasks, task_weights = auto_scheduler.extract_tasks(mod, params, target)
+
+    for idx, task in enumerate(tasks):
+        print("========== Task %d  (workload key: %s) ==========" % (idx, task.workload_key))
+        print(task.compute_dag)
+
+    log_file = "maskrcnn.log"
+    measure_ctx = auto_scheduler.LocalRPCMeasureContext(repeat=1, min_repeat_ms=300, timeout=10)
+
+    tuner = auto_scheduler.TaskScheduler(tasks, task_weights)
+    tune_option = auto_scheduler.TuningOptions(
+        num_measure_trials=20000,  # change this to 20000 to achieve the best performance
+        runner=measure_ctx.runner,
+        measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
+    )
+
+    tuner.tune(tune_option)
+
+
+def bench_tvm():
+    ######################################################################
+    # Import the graph to Relay
+    # -------------------------
+    input_name = "input0"
+    shape_list = [(input_name, input_shape)]
+    mod, params = relay.frontend.from_pytorch(script_module, shape_list)
+
+    target = "cuda"
+
+    # log_file = "../auto_scheduler/resnet-50-NHWC-B1.json"
+    # with auto_scheduler.ApplyHistoryBest(log_file):
+    with tvm.transform.PassContext(opt_level=3, disabled_pass=["FoldScaleAxis"]):
+        vm_exec = relay.vm.compile(mod, target=target, params=params)
+
+    ######################################################################
+    # Inference with Relay VM
+    # -----------------------
+    ctx = tvm.context(target, 0)
+    # vm = profiler_vm.VirtualMachineProfiler(vm_exec, ctx)
+    vm = VirtualMachine(vm_exec, ctx)
+    vm.set_input("main", **{input_name: img})
+    vm.run()
+    # print("\n{}".format(vm.get_stat()))
+
+    ftimer = vm.module.time_evaluator("invoke", ctx, number=1, repeat=num_iters)
+    print(ftimer("main"))
+
+
+# benchmark_torch(model, inp, num_iters)
+# bench_tvm()
+auto_schedule()
