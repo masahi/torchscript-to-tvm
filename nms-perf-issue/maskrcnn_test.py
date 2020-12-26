738b965905990e2b6bb9b15519a645ac82e1d22e
Masahiro Masuda <masahi129@gmail.com>
2020-12-26 17:56:35 +0900

    working on detr

--- nms-perf-issue/maskrcnn_test.py
+++ nms-perf-issue/maskrcnn_test.py
@@ -1,5 +1,6 @@
 import tvm
-from tvm import relay
+from tvm import relay, auto_scheduler
+from tvm.relay.frontend.pytorch_utils import NMSRewrite
 from tvm.contrib.download import download
 from tvm.runtime.vm import VirtualMachine
 from tvm.relay.dataflow_pattern import *
@@ -59,95 +60,6 @@ def get_input():
     return img
 
 
-def batched_nms_pattern(boxes, scores, idxs, iou_threshold):
-    one = is_constant()
-    zero = is_constant()
-
-    # %1796 = expand_dims(%1795, axis=-1);
-    score_expand_dims = is_op("expand_dims")(scores)
-
-    # %1824 = cast(%1823, dtype="float32");
-    cast = is_op("cast")(idxs)
-    mx = is_op("max")(boxes)
-    add = is_op("add")(mx, one)
-    mul = is_op("multiply")(cast, add)
-
-    # %1828 = cast_like(0, meta[relay.Constant][127]);
-    cast_like = is_op("cast_like")(zero, is_constant())
-    less = is_op("less")(is_constant(), cast_like)
-    shape_of = is_op("shape_of")(mul)
-    cast_like = is_op("cast_like")(shape_of, is_constant())
-    add = is_op("add")(is_constant(), cast_like)
-    where = is_op("where")(less, add, is_constant())
-    shape_of = is_op("shape_of")(mul)
-    cast = is_op("cast")(shape_of)
-
-    # %1836 = dyn.strided_slice(%1827, %1833, %1835, meta[relay.Constant][128], begin=None, end=None, strides=None);
-    dyn_strided_slice = is_op("dyn.strided_slice")(mul, where, cast, is_constant())
-
-    expand_dims = is_op("expand_dims")(dyn_strided_slice)
-    add = is_op("add")(boxes, expand_dims)
-    tup = is_tuple([score_expand_dims, add])
-    concat = is_op("concatenate")(tup)
-    expand_dims = is_op("expand_dims")(concat)
-
-    # %1842 = vision.get_valid_counts(%1841, -1f, meta[relay.attrs.GetValidCountsAttrs][1]);
-    get_valid_counts_out = is_op("vision.get_valid_counts")(expand_dims, is_constant())
-    data = is_tuple_get_item(get_valid_counts_out, 1)
-    valid_counts = is_tuple_get_item(get_valid_counts_out, 0)
-    indices = is_tuple_get_item(get_valid_counts_out, 2)
-    return is_op("vision.non_max_suppression")(
-        data, valid_counts, indices, is_constant(), iou_threshold
-    )
-
-
-def convert_batched_nms(boxes, scores, idxs, iou_thres):
-    scores = relay.expand_dims(scores, axis=-1, num_newaxis=1)
-    idxs = relay.expand_dims(idxs, axis=-1, num_newaxis=1)
-    idxs = relay.cast(idxs, "float32")
-    data = relay.concatenate([idxs, scores, boxes], -1)
-    data = relay.expand_dims(data, 0, 1)
-    ct, data, indices = relay.op.vision.get_valid_counts(
-        data, score_threshold=-1.0, id_index=0, score_index=1
-    )
-    top_k = max_out_size = -1
-    out = relay.op.vision.non_max_suppression(
-        data=data,
-        valid_count=ct,
-        indices=indices,
-        max_output_size=max_out_size,
-        iou_threshold=iou_thres,
-        force_suppress=True,
-        top_k=top_k,
-        coord_start=1,
-        score_index=1,
-        id_index=0,
-        return_indices=True,
-        invalid_to_bottom=False,
-    )
-    return out.tuple_value
-
-
-class NMSRewrite(DFPatternCallback):
-    def __init__(self):
-        super().__init__()
-        # exprs I want to extract
-        self.boxes = wildcard()
-        self.scores = wildcard()
-        self.idxs = wildcard()
-        self.iou_threshold = wildcard()
-
-        self.pattern = batched_nms_pattern(
-            self.boxes, self.scores, self.idxs, self.iou_threshold
-        )
-
-    def callback(self, pre, post, node_map):
-        print("matched")
-        boxes = node_map[self.boxes][0]
-        scores = node_map[self.scores][0]
-        idxs = node_map[self.idxs][0]
-        iou_thres = node_map[self.iou_threshold][0]
-        return convert_batched_nms(boxes, scores, idxs, iou_thres)
 
 
 input_name = "input0"
@@ -171,38 +83,36 @@ img = get_input()
 #     fo.write(tvm.ir.save_json(mod))
 # with open("maskrcnn.params", "wb") as fo:
 #     fo.write(relay.save_param_dict(params))
-# print(mod["main"])
+
 with open("maskrcnn_mod.json", "r") as fi:
     mod = tvm.ir.load_json(fi.read())
 with open("maskrcnn.params", "rb") as fi:
     params = relay.load_param_dict(fi.read())
 
 mod["main"] = rewrite(NMSRewrite(), mod["main"])
-# print(mod["main"])
+print(mod["main"])
 
-target = "llvm"
+# with tvm.transform.PassContext(opt_level=3, disabled_pass=["FoldScaleAxis"]):
+#     vm_exec = relay.vm.compile(mod, target=target, params=params)
 
-with tvm.transform.PassContext(opt_level=3, disabled_pass=["FoldScaleAxis"]):
-    vm_exec = relay.vm.compile(mod, target=target, params=params)
+# ######################################################################
+# # Inference with Relay VM
+# # -----------------------
+# ctx = tvm.cpu()
+# vm = VirtualMachine(vm_exec, ctx)
+# vm.set_input("main", **{input_name: img})
+# tvm_res = vm.run()
 
-######################################################################
-# Inference with Relay VM
-# -----------------------
-ctx = tvm.cpu()
-vm = VirtualMachine(vm_exec, ctx)
-vm.set_input("main", **{input_name: img})
-tvm_res = vm.run()
+# ######################################################################
+# # Get boxes with score larger than 0.9
+# # ------------------------------------
+# score_threshold = 0.9
+# boxes = tvm_res[0].asnumpy().tolist()
+# valid_boxes = []
+# for i, score in enumerate(tvm_res[1].asnumpy().tolist()):
+#     if score > score_threshold:
+#         valid_boxes.append(boxes[i])
+#     else:
+#         break
 
-######################################################################
-# Get boxes with score larger than 0.9
-# ------------------------------------
-score_threshold = 0.9
-boxes = tvm_res[0].asnumpy().tolist()
-valid_boxes = []
-for i, score in enumerate(tvm_res[1].asnumpy().tolist()):
-    if score > score_threshold:
-        valid_boxes.append(boxes[i])
-    else:
-        break
-
-print("Get {} valid boxes".format(len(valid_boxes)))
+# print("Get {} valid boxes".format(len(valid_boxes)))
--- nms-perf-issue/test_batched_nms.py
+++ nms-perf-issue/test_batched_nms.py
@@ -3,14 +3,92 @@ import torchvision
 import numpy as np
 import tvm
 from tvm import relay
+from tvm.relay import op
 from tvm.runtime.vm import VirtualMachine
-
-
-iou_threshold = np.asscalar(np.load("iou_threshold.npy")[0])
+from tvm.relay.dataflow_pattern import *
 
 
 def torch_nms(boxes, scores, idxs):
-    return torchvision.ops.batched_nms(boxes, scores, idxs, iou_threshold)
+    return torchvision.ops.batched_nms(boxes, scores, idxs, 0.7)
+
+
+def batched_nms_pattern(boxes, scores, idxs, iou_threshold, num_boxes, indices):
+    one = is_constant()
+    zero = is_constant()
+
+    # %1824 = cast(%1823, dtype="float32");
+    cast = is_op("cast")(idxs)
+    mx = is_op("max")(boxes)
+    add = is_op("add")(mx, one)
+    mul = is_op("multiply")(cast, add)
+
+    dyn_strided_slice = is_op("strided_slice")(mul)
+    expand_dims = is_op("expand_dims")(dyn_strided_slice)
+    add = is_op("add")(boxes, expand_dims)
+
+    score_expand_dims = is_op("expand_dims")(scores)
+
+    tup = is_tuple([score_expand_dims, add])
+    concat = is_op("concatenate")(tup)
+    data = is_op("expand_dims")(concat)
+
+    return is_op("vision.non_max_suppression")(
+        data, num_boxes, indices, is_constant(), iou_threshold
+    )
+
+
+def convert_batched_nms(boxes, scores, idxs, iou_thres, num_boxes, indices):
+    scores = op.expand_dims(scores, axis=-1, num_newaxis=1)
+    idxs = op.expand_dims(idxs, axis=-1, num_newaxis=1)
+    idxs = op.cast(idxs, "float32")
+    data = op.concatenate([idxs, scores, boxes], -1)
+    data = op.expand_dims(data, 0, 1)
+    top_k = max_out_size = -1
+    out = op.vision.non_max_suppression(
+        data=data,
+        valid_count=num_boxes,
+        indices=indices,
+        max_output_size=max_out_size,
+        iou_threshold=iou_thres,
+        force_suppress=False,
+        top_k=top_k,
+        coord_start=2,
+        score_index=1,
+        id_index=0,
+        return_indices=True,
+        invalid_to_bottom=False,
+    )
+    return out.tuple_value
+
+
+class NMSRewrite(DFPatternCallback):
+    def __init__(self):
+        super().__init__()
+        # exprs I want to extract
+        self.boxes = wildcard()
+        self.scores = wildcard()
+        self.idxs = wildcard()
+        self.iou_threshold = wildcard()
+        self.num_boxes = wildcard()
+        self.indices = wildcard()
+
+        self.pattern = batched_nms_pattern(
+            self.boxes,
+            self.scores,
+            self.idxs,
+            self.iou_threshold,
+            self.num_boxes,
+            self.indices,
+        )
+
+    def callback(self, pre, post, node_map):
+        boxes = node_map[self.boxes][0]
+        scores = node_map[self.scores][0]
+        idxs = node_map[self.idxs][0]
+        iou_thres = node_map[self.iou_threshold][0]
+        num_boxes = node_map[self.num_boxes][0]
+        indices = node_map[self.indices][0]
+        return convert_batched_nms(boxes, scores, idxs, iou_thres, num_boxes, indices)
 
 
 boxes_np = np.load("boxes.npy")
@@ -24,12 +102,17 @@ idxs = torch.from_numpy(idxs_np)
 
 trace = torch.jit.trace(torch_nms, [boxes, scores, idxs])
 
+nbox = boxes_np.shape[0]
 input_name = "input0"
-shape_list = [("boxes", boxes_np.shape), ("scores", scores_np.shape), ("idxs", idxs.shape)]
+shape_list = [("boxes", (nbox, 4)), ("scores", (nbox,)), ("idxs", (nbox,))]
 mod, params = relay.frontend.from_pytorch(trace, shape_list)
 
+# mod["main"] = rewrite(NMSRewrite(), mod["main"])
+
 print(mod["main"])
-# target = "cuda"
+# print("np.unique ids", np.unique(idxs_np))
+
+# target = "llvm"
 
 # with tvm.transform.PassContext(opt_level=3, disabled_pass=["FoldScaleAxis"]):
 #     vm_exec = relay.vm.compile(mod, target=target, params=params)
@@ -40,13 +123,7 @@ print(mod["main"])
 # ctx = tvm.context(target, 0)
 # # vm = profiler_vm.VirtualMachineProfiler(vm_exec, ctx)
 # vm = VirtualMachine(vm_exec, ctx)
-# vm.set_input("main", **{"boxes": boxes_np, "scores": scores_np})
+# vm.set_input("main", **{"boxes": boxes_np, "scores": scores_np, "idxs": idxs_np})
 # tvm_res = vm.run()
-# indices_tvm = tvm_res.asnumpy()
 
-# boxes = boxes.to("cuda")
-# scores = scores.to("cuda")
-# indices_torch = torch_nms(boxes, scores).cpu().numpy()
-
-# print(indices_tvm.shape, indices_torch.shape)
-# print(np.sum(indices_tvm - indices_torch))
+# print(tvm_res.shape)
