e357c424e51476b7320db62ab00f2e0369fc1dac
Masahiro Masuda <masahi129@gmail.com>
2020-12-26 20:39:43 +0900

    post nms topk write working

--- nms-perf-issue/test_batched_nms.py
+++ nms-perf-issue/test_batched_nms.py
@@ -8,8 +8,9 @@ from tvm.runtime.vm import VirtualMachine
 from tvm.relay.dataflow_pattern import *
 
 
-def torch_nms(boxes, scores, idxs):
-    return torchvision.ops.batched_nms(boxes, scores, idxs, 0.7)
+def torch_nms(boxes, scores, idxs, max_out_size=1000):
+    indices = torchvision.ops.batched_nms(boxes, scores, idxs, 0.7)
+    return indices[:max_out_size]
 
 
 def batched_nms_pattern(boxes, scores, idxs, iou_threshold, num_boxes, indices):
@@ -91,6 +92,98 @@ class NMSRewrite(DFPatternCallback):
         return convert_batched_nms(boxes, scores, idxs, iou_thres, num_boxes, indices)
 
 
+def dyn_strided_slice_pattern(inp, end):
+    zero = is_constant()
+    cast_like = is_op("cast_like")(zero, is_constant())
+    less = is_op("less")(is_constant(), cast_like)
+    shape_of = is_op("shape_of")(inp)
+    cast_like = is_op("cast_like")(shape_of, is_constant())
+    add = is_op("add")(is_constant(), cast_like)
+    where = is_op("where")(less, add, is_constant())
+
+    return is_op("dyn.strided_slice")(inp, where, end, is_constant())
+
+
+def topk_after_batch_nms_pattern(
+    cond, true_branch, data, valid_count, indices, iou_threshold
+):
+    batched_nms = is_op("vision.non_max_suppression")(
+        data, valid_count, indices, is_constant(), iou_threshold
+    )
+    indices = is_op("squeeze")(is_tuple_get_item(batched_nms, 0))
+    size = is_op("squeeze")(is_tuple_get_item(batched_nms, 1))
+    dyn_strided_slice = dyn_strided_slice_pattern(indices, size)
+    cast_i64 = is_op("cast")(dyn_strided_slice)
+
+    batched_nms_pattern = is_if(cond, true_branch, cast_i64)
+
+    return is_op("strided_slice")(batched_nms_pattern)
+
+
+def rewrite_batch_nms_with_max_out_size(
+    cond, true_branch, data, valid_count, indices, iou_threshold, post_nms_topk
+):
+    nms_ret = op.vision.non_max_suppression(
+        data=data,
+        valid_count=valid_count,
+        indices=indices,
+        max_output_size=post_nms_topk,
+        iou_threshold=iou_threshold,
+        force_suppress=False,
+        top_k=-1,
+        coord_start=2,
+        score_index=1,
+        id_index=0,
+        return_indices=True,
+        invalid_to_bottom=False,
+    )
+
+    size = op.squeeze(nms_ret[1], axis=[1])
+    data_slice = op.squeeze(nms_ret[0], axis=[0])
+
+    ret = op.strided_slice(
+        data_slice, begin=relay.const([0]), end=size, slice_mode="size"
+    )
+
+    nms_result = op.cast(ret, "int64")
+
+    return relay.If(cond, true_branch, nms_result)
+
+
+class PostNMSTopKRewrite(DFPatternCallback):
+    def __init__(self):
+        super().__init__()
+        # exprs I want to extract
+        self.cond = wildcard()
+        self.true_branch = wildcard()
+        self.data = wildcard()
+        self.valid_count = wildcard()
+        self.indices = wildcard()
+        self.iou_threshold = wildcard()
+
+        self.pattern = topk_after_batch_nms_pattern(
+            self.cond,
+            self.true_branch,
+            self.data,
+            self.valid_count,
+            self.indices,
+            self.iou_threshold,
+        )
+
+    def callback(self, pre, post, node_map):
+        print("matched")
+        post_nms_topk = post.attrs.end[0].value
+        return rewrite_batch_nms_with_max_out_size(
+            node_map[self.cond][0],
+            node_map[self.true_branch][0],
+            node_map[self.data][0],
+            node_map[self.valid_count][0],
+            node_map[self.indices][0],
+            node_map[self.iou_threshold][0],
+            post_nms_topk,
+        )
+
+
 boxes_np = np.load("boxes.npy")
 scores_np = np.load("scores.npy")
 scores_np = scores_np - np.min(scores_np) + 1.0
@@ -107,23 +200,8 @@ input_name = "input0"
 shape_list = [("boxes", (nbox, 4)), ("scores", (nbox,)), ("idxs", (nbox,))]
 mod, params = relay.frontend.from_pytorch(trace, shape_list)
 
-# mod["main"] = rewrite(NMSRewrite(), mod["main"])
+# print(mod["main"])
+mod["main"] = rewrite(NMSRewrite(), mod["main"])
+mod["main"] = rewrite(PostNMSTopKRewrite(), mod["main"])
 
 print(mod["main"])
-# print("np.unique ids", np.unique(idxs_np))
-
-# target = "llvm"
-
-# with tvm.transform.PassContext(opt_level=3, disabled_pass=["FoldScaleAxis"]):
-#     vm_exec = relay.vm.compile(mod, target=target, params=params)
-
-# ######################################################################
-# # Inference with Relay VM
-# # -----------------------
-# ctx = tvm.context(target, 0)
-# # vm = profiler_vm.VirtualMachineProfiler(vm_exec, ctx)
-# vm = VirtualMachine(vm_exec, ctx)
-# vm.set_input("main", **{"boxes": boxes_np, "scores": scores_np, "idxs": idxs_np})
-# tvm_res = vm.run()
-
-# print(tvm_res.shape)
