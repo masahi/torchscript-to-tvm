0ff77fb24f26c323dd87c563c22531d36d8af59f
Masahiro Masuda <masahi@129@gmail.com>
2021-02-23 06:27:02 +0900

    add detr amd result

--- detr/detr_test.py
+++ detr/detr_test.py
@@ -50,39 +50,40 @@ def benchmark_torch(model, inp, num_iters):
 
         print("torch elapsed", (t2 - t1) / num_iters)
 
+
+def get_torch_outputs(model, inp):
+    with torch.no_grad():
+        raw_outputs = model(inp)
+        outputs, _ = torch.jit._flatten(raw_outputs)
+        return [output.cpu().numpy() for output in outputs]
+
+
 num_iters = 50
 
-model = TraceWrapper(detr_resnet50(pretrained=False).eval())
-
+model = TraceWrapper(detr_resnet50(pretrained=True).eval())
 model.eval()
 inp = torch.rand(1, 3, 750, 800)
 
+# model = torchvision.models.resnet50(pretrained=True)
+# model.eval()
+# inp = torch.rand(1, 3, 224, 224)
+
 with torch.no_grad():
     trace = torch.jit.trace(model, inp)
+    torch_res = model(inp)
 
 mod, params = relay.frontend.from_pytorch(trace, [('input', inp.shape)])
 
-with open("detr_mod.json", "w") as fo:
-    fo.write(tvm.ir.save_json(mod))
-with open("detr.params", "wb") as fo:
-    fo.write(relay.save_param_dict(params))
-
 
 def auto_schedule():
-    target = "cuda"
+    target = "vulkan"
 
-    with open("detr_mod.json", "r") as fi:
-        mod = tvm.ir.load_json(fi.read())
-    with open("detr.params", "rb") as fi:
-        params = relay.load_param_dict(fi.read())
+    # with open("detr_mod.json", "r") as fi:
+    #     mod = tvm.ir.load_json(fi.read())
+    # with open("detr.params", "rb") as fi:
+    #     params = relay.load_param_dict(fi.read())
 
-
-    desired_layouts = {'nn.conv2d': ['NHWC', 'default']}
-    seq = tvm.transform.Sequential([relay.transform.ConvertLayout(desired_layouts)])
-
-    with tvm.transform.PassContext(opt_level=3):
-        mod = seq(mod)
-
+    mod, params = relay.frontend.from_pytorch(trace, [('input', inp.shape)])
     tasks, task_weights = auto_scheduler.extract_tasks(mod, params, target)
 
     for idx, task in enumerate(tasks):
@@ -104,27 +105,25 @@ def auto_schedule():
 
 
 def bench_tvm():
-    target = "rocm"
+    target = "vulkan"
 
-    with auto_scheduler.ApplyHistoryBest("logs/detr_rocm.log"):
+    mod, params = relay.frontend.from_pytorch(trace, [('input', inp.shape)])
+
+    with auto_scheduler.ApplyHistoryBest("logs/detr_amdvlk.log"):
         with tvm.transform.PassContext(opt_level=3, config={"relay.backend.use_auto_scheduler": True}):
-            vm_exec = relay.vm.compile(mod, target=target, params=params)
+            json, lib, params = relay.build(mod, target=target, params=params)
 
-    # with tvm.transform.PassContext(opt_level=3):
-    #    vm_exec = relay.vm.compile(mod, target=target, params=params)
-
-    ######################################################################
-    # Inference with Relay VM
-    # -----------------------
     ctx = tvm.context(target, 0)
-    # vm = profiler_vm.VirtualMachineProfiler(vm_exec, ctx)
-    vm = VirtualMachine(vm_exec, ctx)
-    vm.set_input("main", **{"input": inp.numpy()})
-    vm.run()
-    # print("\n{}".format(vm.get_stat()))
+    runtime = tvm.contrib.graph_runtime.create(json, lib, ctx)
+    runtime.set_input(**params)
+    runtime.set_input("input", inp.numpy())
+    runtime.run()
 
-    ftimer = vm.module.time_evaluator("invoke", ctx, number=1, repeat=num_iters)
-    print(ftimer("main"))
+    tvm_results = [runtime.get_output(i).asnumpy() for i in [0, 1]]
+    pt_results = get_torch_outputs(model, inp)
+
+    for pt_res, tvm_res in zip(pt_results, tvm_results):
+        print(np.mean(np.abs(pt_res - tvm_res)))
 
 
 # benchmark_torch(model, inp, num_iters)
