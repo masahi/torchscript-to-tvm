8d3f0fa23ab92205a0951f2d4d77e70e7d4eeead
Masahiro Masuda <masahi129@gmail.com>
2021-12-22 09:18:16 +0900

    add bert onnx export

--- transformers/test_bert.py
+++ transformers/test_bert.py
@@ -191,6 +191,31 @@ def evaluate(args, model, tokenizer, prefix=""):
     return results
 
 
+def export_onnx_model(args, model, tokenizer, onnx_model_path):
+    with torch.no_grad():
+        inputs = {'input_ids':      torch.ones(1,128, dtype=torch.int64),
+                    'attention_mask': torch.ones(1,128, dtype=torch.int64),
+                    'token_type_ids': torch.ones(1,128, dtype=torch.int64)}
+        outputs = model(**inputs)
+
+        symbolic_names = {0: 'batch_size', 1: 'max_seq_len'}
+        torch.onnx.export(model,                                            # model being run
+                    (inputs['input_ids'],                             # model input (or a tuple for multiple inputs)
+                    inputs['attention_mask'],
+                    inputs['token_type_ids']),                                         # model input (or a tuple for multiple inputs)
+                    onnx_model_path,                                # where to save the model (can be a file or file-like object)
+                    opset_version=11,                                 # the ONNX version to export the model to
+                    do_constant_folding=True,                         # whether to execute constant folding for optimization
+                    input_names=['input_ids',                         # the model's input names
+                                'input_mask',
+                                'segment_ids'],
+                    output_names=['output'],                    # the model's output names
+                    dynamic_axes={'input_ids': symbolic_names,        # variable length axes
+                                'input_mask' : symbolic_names,
+                                'segment_ids' : symbolic_names})
+        logger.info("ONNX Model exported to {0}".format(onnx_model_path))
+
+
 def load_and_cache_examples(args, task, tokenizer, evaluate=False):
     if args.local_rank not in [-1, 0] and not evaluate:
         torch.distributed.barrier()  # Make sure only the first process in distributed training process the dataset, and the othersv will use the cache
